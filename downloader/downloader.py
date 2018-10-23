354392699e8cbca4f5e7fbe122c95bf48672cf4f
Suyash458 <suyash.behera458@gmail.com>
2018-10-23 09:53:38 +0530

    Adhere to PEP8

--- downloader/downloader.py
+++ downloader/downloader.py
@@ -16,60 +16,60 @@ class SoundcloudDownloader(object):
 		self.API_V2 = "https://api-v2.soundcloud.com"
 		self.download_count = 0
 		self.session = requests.Session()
-		self.session.mount('http://', adapter = HTTPAdapter(max_retries = 3))
-		self.session.mount('https://', adapter = HTTPAdapter(max_retries = 3))
+		self.session.mount("http://", adapter = HTTPAdapter(max_retries = 3))
+		self.session.mount("https://", adapter = HTTPAdapter(max_retries = 3))
 
-	def getFilename(self, metadata):
-		return self.validateName(f"{metadata['artist']}-{metadata['title']}.{metadata['format']}")
+	def get_filename(self, metadata):
+		return self.validate_name(f"{metadata['artist']}-{metadata['title']}.{metadata['format']}")
 
-	def getTrackUrl(self, track):
-		if track['downloadable']: 
-			return (f"{track['download_url']}?client_id={secret}", track.get('original_format', 'mp3'))
-		if track['streamable']: 
-			if 'stream_url' in track:
-				return (f"{track['stream_url']}?client_id={secret}", 'mp3')
-			for transcoding in track['media']['transcodings']:
-				if transcoding['format']['protocol'] == 'progressive':
-					r = self.session.get(transcoding['url'], params={'client_id': secret})
-					return (json.loads(r.text)['url'] , 'mp3')
+	def get_track_url(self, track):
+		if track["downloadable"]: 
+			return (f"{track['download_url']}?client_id={secret}", track.get("original_format", "mp3"))
+		if track["streamable"]: 
+			if "stream_url" in track:
+				return (f"{track['stream_url']}?client_id={secret}", "mp3")
+			for transcoding in track["media"]["transcodings"]:
+				if transcoding["format"]["protocol"] == "progressive":
+					r = self.session.get(transcoding["url"], params={"client_id": secret})
+					return (json.loads(r.text)["url"] , "mp3")
 
-	def getTrackMetadata(self, track):
-		artist = 'unknown'
-		if 'publisher_metadata' in track and track['publisher_metadata']:
-			artist = track['publisher_metadata'].get('artist', '')
-		elif 'user' in track or not artist:
-			artist = track['user']['username']
-		url, fileFormat = self.getTrackUrl(track)
+	def get_track_metadata(self, track):
+		artist = "unknown"
+		if "publisher_metadata" in track and track["publisher_metadata"]:
+			artist = track["publisher_metadata"].get("artist", "")
+		elif "user" in track or not artist:
+			artist = track["user"]["username"]
+		url, fileFormat = self.get_track_url(track)
 		return {
-			'title': str(track.get('title', track['id'])),
-			'artist': artist,
-			'year': str(track.get('release_year', '')),
-			'genre': str(track.get('genre', '')),
-			'format': fileFormat,
-			'download_url': url,
-			'artwork_url': track['artwork_url']
+			"title": str(track.get("title", track["id"])),
+			"artist": artist,
+			"year": str(track.get("release_year", "")),
+			"genre": str(track.get("genre", "")),
+			"format": fileFormat,
+			"download_url": url,
+			"artwork_url": track["artwork_url"]
 		}
 
-	def getSingleTrack(self, track):
+	def get_single_track(self, track):
 		if isinstance(track, resource.Resource): track = track.fields()
-		metadata = self.getTrackMetadata(track)
-		filename = self.getFilename(metadata)
-		download_file(filename, metadata['download_url'])
+		metadata = self.get_track_metadata(track)
+		filename = self.get_filename(metadata)
+		download_file(filename, metadata["download_url"])
 		try:
 			tag_file(filename, metadata)
 		except:
-			if os.path.isfile('artwork.jpg'): os.remove('artwork.jpg')
+			if os.path.isfile("artwork.jpg"): os.remove("artwork.jpg")
 		self.download_count += 1
 
-	def getMultipleTracks(self, tracks):
-		for _, track in filter(lambda x: self.checkTrackNumber(x[0]), enumerate(tracks)):
-			self.getSingleTrack(track)
+	def get_multiple_tracks(self, tracks):
+		for _, track in filter(lambda x: self.check_track_number(x[0]), enumerate(tracks)):
+			self.get_single_track(track)
 
-	def getPlaylist(self, playlist):
-		print(f'{len(playlist.tracks)} tracks found in playlist')
-		self.getMultipleTracks(playlist.tracks)
+	def get_playlist(self, playlist):
+		print(f"{len(playlist.tracks)} tracks found in playlist")
+		self.get_multiple_tracks(playlist.tracks)
 	
-	def checkTrackNumber(self, index):
+	def check_track_number(self, index):
 		if self.download_count == self.args.limit:
 			return False
 		if self.args.include and index + 1 in self.args.include:
@@ -81,97 +81,97 @@ class SoundcloudDownloader(object):
 				return False
 		return True
 
-	def getUploadedTracks(self, user):
-		spinner = Halo(text='Fetching uploads')
+	def get_uploaded_tracks(self, user):
+		spinner = Halo(text="Fetching uploads")
 		spinner.start()			
-		tracks = self.client.get('/tracks', id=user.id)
+		tracks = self.client.get("/tracks", id=user.id)
 		spinner.stop()
-		print(f'Found {len(tracks)} uploads')
-		self.getMultipleTracks(tracks)          
+		print(f"Found {len(tracks)} uploads")
+		self.get_multiple_tracks(tracks)          
 
-	def getRecommendedTracks(self, track, no_of_tracks=10):
+	def get_recommended_tracks(self, track, no_of_tracks=10):
 		params = {
-			'client_id': secret,
-			'limit': no_of_tracks,
-			'offset': 0
+			"client_id": secret,
+			"limit": no_of_tracks,
+			"offset": 0
 		}		
-		spinner = Halo(text=f'Fetching tracks similar to {track.title}')
+		spinner = Halo(text=f"Fetching tracks similar to {track.title}")
 		spinner.start()	
-		recommended_tracks_url = f'{self.API_V2}/tracks/{track.id}/related'
+		recommended_tracks_url = f"{self.API_V2}/tracks/{track.id}/related"
 		r = self.session.get(recommended_tracks_url, params=params)
 		spinner.stop()
-		tracks = json.loads(r.text)['collection']
-		self.getMultipleTracks(tracks)
+		tracks = json.loads(r.text)["collection"]
+		self.get_multiple_tracks(tracks)
 
-	def getLikedTracks(self):
-		spinner = Halo(text='Fetching liked tracks')
+	def get_liked_tracks(self):
+		spinner = Halo(text="Fetching liked tracks")
 		spinner.start()		
-		liked_tracks = self.client.get('/resolve', url=self.url + '/likes')
+		liked_tracks = self.client.get("/resolve", url=self.url + "/likes")
 		spinner.stop()
-		print(f'{len(liked_tracks)} liked track(s) found')
-		self.getMultipleTracks(liked_tracks)
+		print(f"{len(liked_tracks)} liked track(s) found")
+		self.get_multiple_tracks(liked_tracks)
 
-	def validateName(self, name):
-		return re.sub('[\\/:*"?<>|]', '_', name)
+	def validate_name(self, name):
+		return re.sub('[\\/:*"?<>|]', "_", name)
 
-	def getChartedTracks(self, kind, no_of_tracks=10):
+	def get_charted_tracks(self, kind, no_of_tracks=10):
 		url_params = {
-			'limit': no_of_tracks,
-			'genre': 'soundcloud:genres:' + self.args.genre,
-			'kind': kind,
-			'client_id': secret
+			"limit": no_of_tracks,
+			"genre": "soundcloud:genres:" + self.args.genre,
+			"kind": kind,
+			"client_id": secret
 		}
-		url = '{}/charts'.format(self.API_V2)
+		url = "{}/charts".format(self.API_V2)
 		tracks = []
-		spinner = Halo(text=f'Fetching {no_of_tracks} {kind} tracks')
+		spinner = Halo(text=f"Fetching {no_of_tracks} {kind} tracks")
 		spinner.start()
 		while len(tracks) < no_of_tracks:
 			response = self.session.get(url, params=url_params)
 			json_payload = json.loads(response.text)
-			tracks += json_payload['collection']
-			url = json_payload['next_href']
+			tracks += json_payload["collection"]
+			url = json_payload["next_href"]
 		spinner.stop()
-		tracks = map(lambda x: x['track'], tracks[:no_of_tracks])
-		self.getMultipleTracks(tracks)
+		tracks = map(lambda x: x["track"], tracks[:no_of_tracks])
+		self.get_multiple_tracks(tracks)
 
 	def main(self):
 		os.chdir(self.dirname)
 		if self.args.top:
-			self.getChartedTracks('top')
+			self.get_charted_tracks("top")
 		if self.args.new:
-			self.getChartedTracks('trending')
-		spinner = Halo(text='Resolving URL')
+			self.get_charted_tracks("trending")
+		spinner = Halo(text="Resolving URL")
 		spinner.start()		
-		data = self.client.get('/resolve', url=self.url) if self.url else None
+		data = self.client.get("/resolve", url=self.url) if self.url else None
 		spinner.stop()
 		if isinstance(data, resource.Resource):
-			if data.kind == 'user':
+			if data.kind == "user":
 				print("User profile found")
-				folder = self.validateName(data.username)
+				folder = self.validate_name(data.username)
 				if not os.path.isdir(folder): os.mkdir(folder)
 				os.chdir(os.path.join(os.getcwd(), folder))
 				print("Saving in: " + os.getcwd())
 				if self.args.all or self.args.likes:
-					self.getLikedTracks()
+					self.get_liked_tracks()
 				if not self.args.likes:
-					self.getUploadedTracks(data)
-			elif data.kind == 'track':
+					self.get_uploaded_tracks(data)
+			elif data.kind == "track":
 					print("Single track found")
 					print("Saving in: " + os.getcwd())
 					if self.args.similar:
-						self.getRecommendedTracks(data)
-					self.getSingleTrack(data)
-			elif data.kind == 'playlist':
+						self.get_recommended_tracks(data)
+					self.get_single_track(data)
+			elif data.kind == "playlist":
 				print("Single playlist found.")
-				folder = self.validateName(data.user['username'])
+				folder = self.validate_name(data.user["username"])
 				if not os.path.isdir(folder):
 					os.mkdir(folder)
 				os.chdir(os.path.join(os.getcwd(), str(folder)))
-				self.getPlaylist(data)
+				self.get_playlist(data)
 		elif isinstance(data, resource.ResourceList):
-			if data[0].kind == 'playlist':
+			if data[0].kind == "playlist":
 				print("%d playlists found" % (len(data)))
 				for playlist in data:
-					self.getPlaylist(playlist)
-			elif data[0].kind == 'track':
-				self.getMultipleTracks(data)
+					self.get_playlist(playlist)
+			elif data[0].kind == "track":
+				self.get_multiple_tracks(data)
