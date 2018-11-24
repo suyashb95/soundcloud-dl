f0095b7e745f3ac1feaa1a7b5a510fff26a4bc47
Suyash458 <suyash.behera458@gmail.com>
2018-11-24 22:26:26 +0530

    update setup.py and readme

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open('readme.md', 'r') as readme:
 
 setup(
   name = 'soundcloud_dl',
-  version = '0.1.19',
+  version = '0.1.20',
   description = 'A tool to download tracks from soundcloud.com',
   long_description = long_desc,
-  long_description_content_type='text/markdown',  
+  long_description_content_type='text/markdown',
   packages = ['','downloader'],
   author = 'Suyash Behera',
   author_email = 'sne9x@outlook.com',
-  url = 'https://github.com/Suyash458/soundcloud-dl', 
-  download_url = 'https://github.com/Suyash458/soundcloud-dl/archive/master.zip', 
+  url = 'https://github.com/Suyash458/soundcloud-dl',
+  download_url = 'https://github.com/Suyash458/soundcloud-dl/archive/master.zip',
   keywords = ['Downloader', 'Python', 'soundcloud'],
   install_requires = ['soundcloud','requests','mutagen', 'six', 'halo', 'tqdm'],
   entry_points = {
